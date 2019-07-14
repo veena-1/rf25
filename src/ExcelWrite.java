764b608e5e8c9a82f300dfc6cd86decefe94fbe4
Rupeshkumard <rupesh.talekar81296@gmail.com>
2019-07-14 13:59:40 +0530

    excel prgrm

---
+++ src/ExcelWrite.java
@@ -0,0 +1,22 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class ExcelWrite {
+public static void main(String[] args) throws EncryptedDocumentException, FileNotFoundException, IOException {
+		
+		String path = "./data/data.xlsx";
+		Workbook wb = WorkbookFactory.create(new FileInputStream(path));
+		
+		wb.getSheet("Sheet1").getRow(1).getCell(0).setCellValue("admin");
+		
+		//To save 
+		wb.write(new FileOutputStream(path));
+
+  }
+}
